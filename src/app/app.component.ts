2f30c829a3a42653d37c0a2387114b6e0c6c11b9
Cleuber Maciel <105437987+cleuber979@users.noreply.github.com>
2024-02-20 10:52:13 -0300

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NewComponent } from './components/new-component/new-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CommonModule],
-   template:`<!--<router-outlet />-->
-   <h1>Curso de Angular</h1>
-   <h2>Componemts</h2>
-   {{title}}
-   `
-   ,
-
+  imports: [CommonModule,RouterOutlet,NewComponent],
+  template:`
+  <!--<router-outlet />-->
+  <h1>Curso de Angula</h1>
+  <app-new-component/>
+  {{title}}
+  `,
 })
 export class AppComponent {
   title = 'meu-primeiro-projeto-latest';
