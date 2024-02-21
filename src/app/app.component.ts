15421900b1f57a0cb994d2ed371c20869c412119
Cleuber Maciel <105437987+cleuber979@users.noreply.github.com>
2024-02-21 10:10:56 -0300

    uso estilos scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,14 @@ import { NewComponent } from './components/new-component/new-component.component
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule,RouterOutlet,NewComponent],
+ styles:[
+  `
+    h1{
+      color:yellow;
+    }
+  `,
+ ],
+
   template:`
   <!--<router-outlet />-->
   <h1>Curso de Angula</h1>
